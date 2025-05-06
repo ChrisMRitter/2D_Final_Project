7222308ae46381a926f46931a6268459a61f19ae
AlexanderAldama-Apodaca <130171945+AlexanderAldama-Apodaca@users.noreply.github.com>
2025-05-06 09:09:58 -0400

    Update main.cpp
    Added instruction to Start Screen informing the player to use the mouse to aim at enemy ships.

--- src/main.cpp
+++ src/main.cpp
@@ -191,7 +191,7 @@ int main() {
 // Press-Enter-To-Start screen
 // ------------------------------------------------------------------------
 void startScreen(sf::RenderWindow& window, const sf::Font& font) {
-    sf::Text prompt("Press Enter to Start\nPress P to Pause\nPress Esc to Quit\nPress A and D to Move\nPress Space to Shoot", font, 50);
+    sf::Text prompt("Press Enter to Start\nPress P to Pause\nPress Esc to Quit\nPress A and D to Move\nPress Space to Shoot\nUse Mouse to Aim", font, 50);
     prompt.setFillColor(sf::Color::White);
     prompt.setPosition(window.getSize().x / 2.f - prompt.getGlobalBounds().width / 2.f,
         window.getSize().y / 2.f - prompt.getGlobalBounds().height / 2.f);
