b4431a7d44d228638493e42a0c71214bba2f0817
AlexanderAldama-Apodaca <130171945+AlexanderAldama-Apodaca@users.noreply.github.com>
2025-05-14 13:25:16 -0400

    Update enemy.cpp
    Added comment marking where enemy death sounds should probably go

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -59,5 +59,5 @@ sf::FloatRect Enemy::getGlobalBounds() const {
 int Enemy::getHealth() const { return health; }
 void Enemy::setHealth(int h) { health = h; }
 int Enemy::takeDamage() { health--; if (health <= 0) alive = false; return health; }
-void Enemy::killEnemy() { alive = false; }
+void Enemy::killEnemy() { alive = false; } // probably add enemy death sounds here
 bool Enemy::isAlive() const { return alive; }
