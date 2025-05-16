f2e5ad776b31687eef67a0c9ac2f07d2dc95c56a
AlexanderAldama-Apodaca <130171945+AlexanderAldama-Apodaca@users.noreply.github.com>
2025-05-15 22:18:47 -0400

    Update hitbox.cpp
    Added descriptive comments to hitbox.cpp

--- src/hitbox.cpp
+++ src/hitbox.cpp
@@ -1,25 +1,33 @@
-// This is a custom hitbox class, made to better show off collision and because
-// the default size of the sprites I chose bugs me.
-
+/*
+This is a custom hitbox class made to better display collision
+*/
 #include "hitbox.h"
 
-// Constructor for the Hitbox class
+/*
+Constructor for the Hitbox class
+- size: dimensions of the hitbox
+- color: optional fill color for visualization (default is transparent)
+*/
 Hitbox::Hitbox(sf::Vector2f size, sf::Color color) {
-  box.setSize(size);
-  box.setOrigin(size / 2.f); // center origin
-  box.setFillColor(color);
+  box.setSize(size); // Set the size of the hitbox
+  box.setOrigin(size / 2.f); // Center the origin for easier alignment
+  box.setFillColor(color); // Set the fill color (can be transparent or visible for debugging)
 }
 
-// Set the position of the hitbox
+// Set the position of the hitbox in the game world
 void Hitbox::setPosition(const sf::Vector2f &pos) { box.setPosition(pos); }
-// Set the size of the hitbox
+
+// Set a new size for the hitbox and re-center its origin
 void Hitbox::setSize(const sf::Vector2f &size) {
   box.setSize(size);
   box.setOrigin(size / 2.f);
 }
-// Set the color of the hitbox
+
+// Set the fill color of the hitbox (useful for debugging)
 void Hitbox::setColor(const sf::Color &color) { box.setFillColor(color); }
-//  Get the bounds of the hitbox
+
+//  Get the global bounds of the hitbox (used for collision detection)
 sf::FloatRect Hitbox::getBounds() const { return box.getGlobalBounds(); }
-// Draw the hitbox to the window
+
+// Draw the hitbox to the window (only visible if it has a non-transparent color)
 void Hitbox::draw(sf::RenderWindow &window) { window.draw(box); }
