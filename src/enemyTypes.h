18a73c3f00f69d243d2f16cb27056c522094a18d
AlexanderAldama-Apodaca <130171945+AlexanderAldama-Apodaca@users.noreply.github.com>
2025-05-15 22:30:22 -0400

    Update enemyTypes.h
    Added comments to enemyTypes.h

--- src/enemyTypes.h
+++ src/enemyTypes.h
@@ -4,9 +4,9 @@
 #define ENEMY_TYPES_H
 
 enum class EnemyType {
-    GREEN_ENEMY,  // Small, weak, fast (Level 1)
-    RED_ENEMY,    // Larger, tougher, faster (Level 2)
-    MOTHERSHIP    // Boss (Level 3)
+    GREEN_ENEMY,  // Small, weak, fast enemy (introduced in Level 1)
+    RED_ENEMY,    // Larger, tougher, faster enemy (introduced in Level 2)
+    MOTHERSHIP    // Boss (introduced in Level 3)
 };
 
 #endif // ENEMY_TYPES_H
